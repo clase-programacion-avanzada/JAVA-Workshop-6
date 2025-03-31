8164f831d458834f98f74b7ed3341eaf8c1a23c8
Diego Fernando Mora Velasco <difemove@gmail.com>
2025-03-31 18:01:20 -0500

    enunciado

--- src/main/java/com/javeriana/Main.java
+++ src/main/java/com/javeriana/Main.java
@@ -1,10 +1,23 @@
 package com.javeriana;
 
+import java.util.Scanner;
+
 public class Main {
     
 
     public static void main(String[] args) {
-        
+
+    // This is the way to ask for the user input
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please enter the name of the artist:");
+        String artistName = scanner.nextLine();
+
+        System.out.println("Customer entered: " + artistName);
+
+        //You should create a menu to ask the user what he wants to do
+
     }
   
 }
